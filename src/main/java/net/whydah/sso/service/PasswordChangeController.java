8b30973270b2a3cf9eb12341c10a75dd8d6622e0
Sherriff <erik-dev@fjas.no>
2013-08-18 19:24:45 +0200

    Add trace logging to PasswordChangeController.

--- src/main/java/net/whydah/sso/service/PasswordChangeController.java
+++ src/main/java/net/whydah/sso/service/PasswordChangeController.java
@@ -5,6 +5,8 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import net.whydah.sso.service.config.AppConfig;
 import net.whydah.sso.service.data.PasswordChangeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,7 @@ import java.net.URI;
  */
 @Controller
 public class PasswordChangeController {
+    private static final Logger log = LoggerFactory.getLogger(PasswordChangeController.class);
     private static final Client uibClient = Client.create();
     private URI uibServiceUri;
 
@@ -36,6 +39,7 @@ public class PasswordChangeController {
 
     @RequestMapping("/resetpassword")
     public String resetpassword(HttpServletRequest request, Model model) {
+        log.trace("resetpassword was called");
         String user = request.getParameter("user");
         if(user == null) {
             return "resetpassword";
@@ -45,7 +49,7 @@ public class PasswordChangeController {
         ClientResponse response = uibWR.type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
         if(response.getStatus() != ClientResponse.Status.OK.getStatusCode()) {
             String error = response.getEntity(String.class);
-            System.out.println(error);
+            log.error(error);
             model.addAttribute("error", error);
             return "resetpassword";
         }
@@ -54,6 +58,7 @@ public class PasswordChangeController {
 
     @RequestMapping("/changepassword/*")
     public String changePasswordFromLink(HttpServletRequest request, Model model) {
+        log.trace("changePasswordFromLink was called");
         PasswordChangeToken passwordChangeToken = getTokenFromPath(request);
         model.addAttribute("user", passwordChangeToken.getUser());
         model.addAttribute("token", passwordChangeToken.getToken());
@@ -66,17 +71,18 @@ public class PasswordChangeController {
 
     @RequestMapping("/dochangepassword/*")
     public String doChangePasswordFromLink(HttpServletRequest request, Model model) {
+        log.trace("doChangePasswordFromLink was called");
         PasswordChangeToken passwordChangeToken = getTokenFromPath(request);
         String newpassword = request.getParameter("newpassword");
-        System.out.println(newpassword);
+        //System.out.println(newpassword);
         WebResource uibWR = uibClient.resource(uibServiceUri).path("/useradmin/users/" + passwordChangeToken.getUser() + "/newpassword/" + passwordChangeToken.getToken());
         System.out.println("kaller " + uibWR.getURI());
         ClientResponse response = uibWR.type(MediaType.APPLICATION_JSON).post(ClientResponse.class,"{\"newpassword\":\"" + newpassword + "\"}");
         model.addAttribute("user", passwordChangeToken.getUser());
         if(response.getStatus() != ClientResponse.Status.OK.getStatusCode()) {
             String error = response.getEntity(String.class);
-            System.out.println(response);
-            System.out.println(error);
+            //System.out.println(response);
+            log.error(error);
             model.addAttribute("error", error);
             model.addAttribute("token", passwordChangeToken.getToken());
             return "changepassword";

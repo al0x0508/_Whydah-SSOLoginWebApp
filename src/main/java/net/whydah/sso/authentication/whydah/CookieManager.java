eb7ae1511b7ad6da67a2f3cafe1b3ef9768c2317
Sherriff <erik-dev@fjas.no>
2014-11-07 14:04:41 +0100

    #22 - let setDomain for user token cookie if cookiedomain is not set (allow only same server).

--- src/main/java/net/whydah/sso/authentication/whydah/CookieManager.java
+++ src/main/java/net/whydah/sso/authentication/whydah/CookieManager.java
@@ -15,7 +15,7 @@ public class CookieManager {
     private static final String LOGOUT_COOKIE_VALUE = "logout";
     private static final Logger logger = LoggerFactory.getLogger(CookieManager.class);
 
-    private static String cookiedomain;
+    private static String cookiedomain = null;
 
     private CookieManager() {
     }
@@ -24,7 +24,6 @@ public class CookieManager {
         try {
             cookiedomain = AppConfig.readProperties().getProperty("cookiedomain");
         } catch (IOException e) {
-            cookiedomain = ".whydah.net";
             logger.warn("AppConfig.readProperties failed. cookiedomain was set to {}", cookiedomain, e);
         }
     }
@@ -35,7 +34,9 @@ public class CookieManager {
         // cookie.setMaxAge(maxAge);
         cookie.setMaxAge(365 * 24 * 60 * 60);
         cookie.setPath("/");
-        cookie.setDomain(cookiedomain);
+        if (cookiedomain != null && !cookiedomain.isEmpty()) {
+            cookie.setDomain(cookiedomain);
+        }
         cookie.setValue(userTokenId);
         // cookie.setSecure(true);
         logger.trace("Created cookie with name={}, domain={}, value/userTokenId={}, maxAge={}, secure={}",
@@ -60,14 +61,6 @@ public class CookieManager {
         }
     }
 
-    public static void setLogoutUserTokenCookie(HttpServletRequest request, HttpServletResponse response) {
-        Cookie userTokenCookie = getUserTokenCookie(request);
-        if (userTokenCookie != null) {
-            userTokenCookie.setValue(LOGOUT_COOKIE_VALUE);
-            response.addCookie(userTokenCookie);
-        }
-    }
-
     private static Cookie getUserTokenCookie(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
@@ -82,52 +75,11 @@ public class CookieManager {
     }
 
 
-     /*
-    public static String getCookieDomain() {
-        logger.info("CookieDomain: " + cookiedomain);
-        return cookiedomain;
-    }
-    */
-
-    /*
-     public WhydahUserTokenId getUserTokenIdFromCookie(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
-        boolean found = false;
-        WhydahUserTokenId foundTokenId = WhydahUserTokenId.fromTokenId("");
-        if (cookies != null) {
-            logger.trace("getUserTokenIdFromCookie - Found {} cookie(s)", cookies.length);
-            for (Cookie cookie : cookies) {
-                logger.trace("getUserTokenIdFromCookie - Checking cookie:"+cookie.getName());
-                if (!CookieManager.USER_TOKEN_REFERENCE_NAME.equals(cookie.getName())) {
-                    continue;
-                }
-
-                String usertokenId = cookie.getValue();
-                logger.trace("getUserTokenIdFromCookie - Found whydahusertoken cookie, usertokenid={}", usertokenId);
-                if ("logout".equalsIgnoreCase(usertokenId)) {
-
-                    // TODO: should probably clear the logout cookie here?
-
-                    return WhydahUserTokenId.invalidTokenId();
-                }
-                if (tokenServiceClient.verifyUserTokenId(usertokenId)) {
-                    logger.trace("getUserTokenIdFromCookie - usertokenid ok");
-                    foundTokenId = WhydahUserTokenId.fromTokenId(usertokenId);
-                    found = true;
-                } else {
-                    cookie.setMaxAge(0);
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    logger.trace("Cleared cookie with invalid usertokenid");
-                    response.addCookie(cookie);
-                }
-            }
+    public static void setLogoutUserTokenCookie(HttpServletRequest request, HttpServletResponse response) {
+        Cookie userTokenCookie = getUserTokenCookie(request);
+        if (userTokenCookie != null) {
+            userTokenCookie.setValue(LOGOUT_COOKIE_VALUE);
+            response.addCookie(userTokenCookie);
         }
-        if (found) {
-            return foundTokenId;
-        }
-        logger.debug("getUserTokenIdFromCookie - Found no cookies with usertokenid");
-        return WhydahUserTokenId.invalidTokenId();
     }
-    */
 }

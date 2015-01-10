3bd80d8212bba943631dbc28cd104cd0ef030d33
Totto <totto@totto.org>
2015-01-10 22:20:08 +0100

    detach

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/SSOLoginWebApp/ssologinservice.PROD.properties -jar /home/SSOLoginWebApp/SSOLoginWebApp.jar
+nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/SSOLoginWebApp/ssologinservice.PROD.properties -jar /home/SSOLoginWebApp/SSOLoginWebApp.jar &

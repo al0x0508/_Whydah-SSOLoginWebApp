032f22a4dd7fa2edf26c7efeddd29a221e2d1aac
Totto <totto@totto.org>
2015-01-13 19:14:54 +0100

    completed posting signup user to UAS

--- src/main/java/net/whydah/sso/useradmin/NewUserController.java
+++ src/main/java/net/whydah/sso/useradmin/NewUserController.java
@@ -1,5 +1,8 @@
 package net.whydah.sso.useradmin;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import net.whydah.sso.authentication.ModelHelper;
 import net.whydah.sso.authentication.UserCredential;
 import net.whydah.sso.config.AppConfig;
@@ -12,14 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.Properties;
 
 @Controller
 public class NewUserController {
 
     private static final Logger logger = LoggerFactory.getLogger(NewUserController.class);
+    private static final Client uasClient = Client.create();
+    private URI uasServiceUri;
+
     private final TokenServiceClient tokenServiceClient = new TokenServiceClient();
     String LOGOURL = "/sso/images/site-logo.png";
 
@@ -27,6 +36,8 @@ public class NewUserController {
         Properties properties = AppConfig.readProperties();
         String MY_APP_URI = properties.getProperty("myuri");
         LOGOURL = properties.getProperty("logourl");
+        uasServiceUri = UriBuilder.fromUri(AppConfig.readProperties().getProperty("useradminservice")).build();
+
     }
 
     @RequestMapping("/signup")
@@ -34,12 +45,38 @@ public class NewUserController {
         logger.trace("/signup entry");
         String username = request.getParameter("username");
         String email = request.getParameter("email");
-        String firstname = request.getParameter("firstname");
-        String lastname = request.getParameter("lastname");
-        String cellphone = request.getParameter("cellphone");
-        if (email!=null && username != null) {
-            logger.info("Requested signup - email: "+email+"  username: "+username+"  firstname: "+firstname+"  lastname: "+lastname+"  cellphone: "+cellphone+" ");
-            // TODO   Post signup-request to UAS
+        String firstName = request.getParameter("firstname");
+        String lastName = request.getParameter("lastname");
+        String cellPhone = request.getParameter("cellphone");
+        if (email != null && username != null) {
+            logger.info("Requested signup - email: " + email + "  username: " + username + "  firstname: " + firstName + "  lastname: " + lastName + "  cellphone: " + cellPhone + " ");
+            String userJson = "UserIdentity{" +
+                    "uid='" + username + '\'' +
+                    ", username='" + username + '\'' +
+                    ", firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", personRef='" + "" + '\'' +
+                    ", email='" + email + '\'' +
+                    ", cellPhone='" + cellPhone + '\'' +
+                    '}';
+            ;
+            try {
+                WebResource uasWR = uasClient.resource(uasServiceUri).path(tokenServiceClient.getMyAppTokenID()).path("userTokenId").path("user");
+                logger.trace("doChangePasswordFromLink was called. Calling UAS with url " + uasWR.getURI());
+
+                ClientResponse uasResponse = uasWR.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, "{\"userXml\":\"" + userJson + "\"}");
+                if (uasResponse.getStatus() != ClientResponse.Status.OK.getStatusCode()) {
+                    String error = uasResponse.getEntity(String.class);
+                    logger.error(error);
+                    model.addAttribute("error", "We were unable to create the requested user at this time. Try different data or later.");
+                }
+
+            } catch (IllegalStateException ise) {
+                logger.info(ise.getMessage());
+            } catch (RuntimeException e) {
+                logger.error("Unkonwn error.", e);
+            }
+
         }
 
         model.addAttribute("logoURL", LOGOURL);

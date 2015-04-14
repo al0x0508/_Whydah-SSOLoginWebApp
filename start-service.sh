d990ea5fc8fe5d21f821a5e5feaa1e819d42474f
Thor Henning Hetland <totto@totto.org>
2015-04-14 16:32:15 +0200

    oops, typo :)

--- start-service.sh
+++ start-service.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #  If IAM_MODE not set, use PROD
-if [ -z "$IAM_MODE" ]
+if [ -z "$IAM_MODE" ]; then
   IAM_MODE=PROD
 fi
 
 # If IAM_CONFOG not set, use PROD properties from local directory
-if [ -z "$IAM_CONFIG" ]
+if [ -z "$IAM_CONFIG" ]; then
   IAM_CONFIG=ssologinservice.PROD.properties
 fi
 
 # If Version is from source, find the artifact
-if [ $Version = "FROM_SOURCE" ]; then 
+if [ "$Version" = "FROM_SOURCE" ]; then 
     # Find the bult artifact
     Version=$(find target/* -name *.jar | grep SNAPSHOT | grep -v original | grep -v lib)
 else

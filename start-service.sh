c278b37174891664ef496185ea73391eec03de58
Thor Henning Hetland <totto@totto.org>
2015-04-14 16:14:21 +0200

    Added some parameter parsing

--- start-service.sh
+++ start-service.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-nohup /usr/bin/java -DIAM_MODE=PROD -DIAM_CONFIG=/home/SSOLoginWebApp/ssologinservice.PROD.properties -jar /home/SSOLoginWebApp/SSOLoginWebApp.jar &
+
+#  If IAM_MODE not set, use PROD
+if [ -z "$IAM_MODE" ]
+  IAM_MODE=PROD
+fi
+
+# If IAM_CONFOG not set, use PROD properties from local directory
+if [ -z "$IAM_CONFIG" ]
+  IAM_CONFIG=ssologinservice.PROD.properties
+fi
+
+# If Version is from source, find the artifact
+if [ $Version = "FROM_SOURCE" ]; then 
+    # Find the bult artifact
+    Version=$(find target/* -name *.jar | grep SNAPSHOT | grep -v original | grep -v lib)
+else
+    Version=SSOLoginWebApp.jar
+fi
+
+
+nohup /usr/bin/java -DIAM_MODE=$IAM_MODE -DIAM_CONFIG=$IAM_CONFIG -jar  $Version &
